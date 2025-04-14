81be308f33c6e95f5057395d17c692939797aafe
Sridevi774 <sridevikuppa7743@gmail.com>
2025-04-14 10:54:05 +0530

    added changes by sridevi

---
+++ script.js
@@ -0,0 +1,23 @@
+
+function showAlert() {
+    alert("You can contact me at sridevikuppa7743@gmail.com");
+  }
+
+window.onload = function() {
+  document.getElementById("myPara").style.textAlign = "right";
+}
+
+  
+function changeBackgroundColor() {
+  document.body.style.backgroundColor = "#e0f7fa";
+}
+  
+function changeTextColor() {
+  document.body.style.color = "#333399";
+}
+  
+function printPage() {
+  window.print();
+}
+  
+  
